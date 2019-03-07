3a7d0a64deeb31674cf31fe570d70d2807759d60
Elizaveta <matronka@mail.ru>
2019-03-07 23:02:01 +0300

    Add classes and fix main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import {random} from './utils.js';
 import createRandomCard from './data.js';
 import createFilterElement from './filter.js';
-import createCard from './card.js';
+import {Card} from './card/card.js';
+import {CardDetails} from './card/card-details.js';
 
 // Filter
 
@@ -13,14 +14,33 @@ const filterElementsArray = [
   [`Favorites`]
 ];
 
+// Card
+
 const cardContainer = document.querySelector(`.films-list .films-list__container`);
 const cardContainersExtra = document.querySelectorAll(`.films-list--extra .films-list__container`);
 
 const showCards = (num, container) => {
   container.innerHTML = ``;
 
+  const cardComponentData = [];
+  const cardComponent = [];
+  const cardComponentDetails = [];
+
   for (let i = 1; i <= num; i++) {
-    container.insertAdjacentHTML(`beforeend`, createCard(createRandomCard()));
+    cardComponentData[i] = createRandomCard();
+    cardComponent[i] = new Card(cardComponentData[i]);
+    cardComponentDetails[i] = new CardDetails(cardComponentData[i]);
+
+    cardComponent[i].onDetails = () => {
+      document.body.appendChild(cardComponentDetails[i].render());
+    };
+
+    cardComponentDetails[i].onDetailsClose = () => {
+      document.body.removeChild(document.querySelector(`.film-details`));
+      cardComponentDetails[i].unrender();
+    };
+
+    container.appendChild(cardComponent[i].render());
 
     const cardsExtra = document.querySelectorAll(`.films-list--extra .film-card`);
     cardsExtra.forEach((cardExtra) => {
